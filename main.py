adcf5969a34c21ce0af8db718136710e68c78115
acenturyandabit <steeven.liu2@gmail.com>
2020-10-08 11:49:54 +1100

    added stevens version

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import cv2
 import asyncio
-import phonesource2
-
-class awaitableVideoCapture:
-    def __init__(self,sourceArg):
-        self.source=cv2.VideoCapture(sourceArg)
-    async def read(self):
-        return self.source.read()
-
-source = awaitableVideoCapture("http://192.168.43.1:8083/video")
-
-def localDisplay(frame, result):
-    cv2.imshow("frame",frame)
-    key=cv2.waitKey(1)
-    pass
-
-displayResult = localDisplay
-#displayResult = CustomWebserver.output
 
 
-async def main():
-    print ("hi")
-    isOK=True
-    while isOK:
-        isOK, frame = await source.read()
-        # do processing
-        if isOK:
-            displayResult(frame, result={})
+source = cv2.VideoCapture("http://192.168.43.1:8083/video")
 
-asyncio.get_event_loop().create_task(main())
-#phonesource2.start()
-asyncio.get_event_loop().run_forever()
+
+isOK=True
+while isOK:
+    isOK, frame = source.read()
+    # do processing
+    if isOK:
+        cv2.imshow("frame",frame)
+        key=cv2.waitKey(1)
+        pass
+
---
+++ stevenTextDetect/main.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import os
+from opencv_text_detection import text_detection as td
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(os.path.join(dir_path,'Portrait_Powerbanks.mp4'))
+
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+    frame  = cv2.resize(frame,(1024,928),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+    frame  = td.text_detection(frame,
+                               east=os.path.join(dir_path, 'opencv_text_detection/frozen_east_text_detection.pb'),
+                               min_confidence=0.5, 
+                               width=1024, 
+                               height=928)
+
+    # Display the resulting frame
+    # cv2.imshow('Frame',frame)
+
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+  # Break the loop
+  else: 
+    break
+  print ("frame")
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
