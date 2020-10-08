10c9bba914ebcabb6480389eb36474709fee097e
Rishi Rangarajan <61214686+rran9235@users.noreply.github.com>
2020-10-08 11:31:23 +0530

    Main function now in main_barcode.py. Cleaned up everything else.
    Code now outputs a deque holding all the strings read from the QR/Barcode scanning. Cleaned up additional code.

--- Img2Barcode/main2.py
+++
@@ -1,8 +0,0 @@
-from Barcode import Barcode_Detect
-import cv2
-
-# Main
-if __name__ == '__main__':
-
-    image = cv2.imread(r"test_images\test1.jpg")
-    Barcode_Detect(image)
--- Img2Barcode/main_barcode.py
+++ Img2Barcode/main_barcode.py
@@ -1,39 +1,64 @@
-# import the necessary packages
+### File Information
+# Team: The Hardest Part
+# SUMO Hackathon 2020
+# Date: 08/10/2020
+#
+# Source: Code derived from work by Adrian Rosebrock
+# https://www.pyimagesearch.com/2018/05/21/an-opencv-barcode-and-qr-code-scanner-with-zbar/
+
+
+# Imports
 from pyzbar import pyzbar
-import argparse
 import cv2
+from collections import deque
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = vars(ap.parse_args())
 
-# load the input image
-image = cv2.imread(args["image"])
+# Function Definition
+def Barcode_Detect (image):
 
-# find the barcodes in the image and decode each of the barcodes
-barcodes = pyzbar.decode(image)
+    # Find barcodes in image (QR/CODE128) and decode each
+    barcodes = pyzbar.decode(image)
 
-# loop over the detected barcodes
-for barcode in barcodes:
-	# extract the bounding box location of the barcode and draw the
-	# bounding box surrounding the barcode on the image
-	(x, y, w, h) = barcode.rect
+    # Initialise deque to hold data
+    Data_Deque = deque()
+    Type_Deque = deque()
 
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	# the barcode data is a bytes object so if we want to draw it on
-	# our output image we need to convert it to a string first
-	barcodeData = barcode.data.decode("utf-8")
-	barcodeType = barcode.type
-	
-    # draw the barcode data and barcode type on the image
-	text = "{} ({})".format(barcodeData, barcodeType)
-	cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.5, (0, 0, 255), 2)
-	# print the barcode type and data to the terminal
-	print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+    # Iterate through each detected barcode
+    for barcode in barcodes:
 
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+        # Extract location of bounding box containing barcode
+        (x, y, w, h) = barcode.rect
+
+        # Draw bounding box around barcode
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # Set barcode data format and decode information
+        barcodeData = barcode.data.decode("utf-8")
+        # Identify type of barcode (QR or CODE128)
+        barcodeType = barcode.type
+
+        # Append data to Deque
+        Data_Deque.append(barcodeData)
+        Type_Deque.append(barcodeType)
+        
+        # Store barcode data in string
+        text = "{} ({})".format(barcodeData, barcodeType)
+        # Write barcode data back onto image
+        cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+            0.5, (0, 0, 255), 2)
+
+    # Display output image
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    
+    return Data_Deque
+
+
+
+# Main Function
+if __name__ == '__main__':
+
+    image = cv2.imread(r"tests\test1.jpg")
+    Out_Deque = Barcode_Detect(image)
+    
+    print(Out_Deque)
